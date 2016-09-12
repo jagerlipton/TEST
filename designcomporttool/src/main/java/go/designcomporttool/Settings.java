71c9ac7a005830fbbaa53059bc184ce75d4f4d1c
test <povidlof@gmail.com>
2016-09-12 09:05:10 +0400

    g

--- designcomporttool/src/main/java/go/designcomporttool/Settings.java
+++ designcomporttool/src/main/java/go/designcomporttool/Settings.java
@@ -2,13 +2,20 @@ package go.designcomporttool;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class Settings extends AppCompatActivity {
-
+    String[] names = { "Скорость", "биты данных", "четность", "стоп биты", "flow control"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        //jhhjhkgkgjhgjhg
-    }
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        lvMain.setAdapter(adapter);
+          }
 }
