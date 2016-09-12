47f21dac1b0710e1e863ae5f2185173e65090f62
work <povidlof@gmail.com>
2016-09-12 13:56:36 +0400

    monday

--- designcomporttool/src/main/java/go/designcomporttool/Settings.java
+++ designcomporttool/src/main/java/go/designcomporttool/Settings.java
@@ -1,21 +1,57 @@
 package go.designcomporttool;
 
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.app.Activity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 
-public class Settings extends AppCompatActivity {
-    String[] names = { "Скорость", "биты данных", "четность", "стоп биты", "flow control"};
+public class Settings extends PreferenceActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        ListView lvMain = (ListView) findViewById(R.id.lvMain);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
-        lvMain.setAdapter(adapter);
-          }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            onCreatePreferenceActivity();
+        } else {
+            onCreatePreferenceFragment();
+        }
+
+    }
+
+    /**
+     * code for Android < 3 (i.e. API lvl < 11)
+     */
+    @SuppressWarnings("deprecation")
+    private void onCreatePreferenceActivity() {
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    /**
+     * code for Android >= 3 (i.e. API lvl >= 11)
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void onCreatePreferenceFragment() {
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFr())
+                .commit();
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class SettingsFr extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+        }
+    }
+
 }
