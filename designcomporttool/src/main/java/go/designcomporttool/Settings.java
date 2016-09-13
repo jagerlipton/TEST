08f25c563b088413b0db3850a4b830b3f6c9c107
test <povidlof@gmail.com>
2016-09-13 16:45:11 +0400

    tuesday

--- designcomporttool/src/main/java/go/designcomporttool/Settings.java
+++ designcomporttool/src/main/java/go/designcomporttool/Settings.java
@@ -15,35 +15,12 @@ public class Settings extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-            onCreatePreferenceActivity();
-        } else {
-            onCreatePreferenceFragment();
-        }
-
-    }
-
-    /**
-     * code for Android < 3 (i.e. API lvl < 11)
-     */
-    @SuppressWarnings("deprecation")
-    private void onCreatePreferenceActivity() {
-        addPreferencesFromResource(R.xml.pref_general);
-    }
-
-    /**
-     * code for Android >= 3 (i.e. API lvl >= 11)
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void onCreatePreferenceFragment() {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFr())
                 .commit();
+
     }
 
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class SettingsFr extends PreferenceFragment
     {
         @Override
