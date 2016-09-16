11746c75f75d8985c6e4fd144743aef436cab243
test <povidlof@gmail.com>
2016-09-16 14:46:03 +0400

    friday

--- designcomporttool/src/main/java/go/designcomporttool/Settings.java
+++ designcomporttool/src/main/java/go/designcomporttool/Settings.java
@@ -1,10 +1,17 @@
 package go.designcomporttool;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+
+
+
+
 
 public class Settings extends PreferenceActivity {
 
@@ -13,8 +20,8 @@ public class Settings extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
 
-
     }
+
 
     public static class MyPreferenceFragment extends PreferenceFragment
     {
@@ -23,7 +30,50 @@ public class Settings extends PreferenceActivity {
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
+            bindPreferenceSummaryToValue(findPreference("comport_bauderate"));
+            bindPreferenceSummaryToValue(findPreference("comport_databit"));
+            bindPreferenceSummaryToValue(findPreference("comport_chet"));
+            bindPreferenceSummaryToValue(findPreference("comport_stopbits"));
+            bindPreferenceSummaryToValue(findPreference("comport_flowcontrol"));
+
         }
+    }
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+
+        private String TAG = Settings.class.getSimpleName();
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                  preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+            } else {
+
+                preference.setSummary(stringValue);
+            }
+            Log.i(TAG, "pref changed : " + preference.getKey() + " " + value);
+            return true;
+        }
+    };
+
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+           sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
     }
 
 }
+
